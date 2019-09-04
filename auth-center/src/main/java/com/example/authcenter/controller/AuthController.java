9aa30126f7b2bc99f78651208aeeed369101e9bd
xujw <xujiawei@quklive.com>
2019-09-04 16:36:59 +0800

    spring-cloud-config

--- auth-center/src/main/java/com/example/authcenter/controller/AuthController.java
+++ auth-center/src/main/java/com/example/authcenter/controller/AuthController.java
@@ -1,11 +1,18 @@
 package com.example.authcenter.controller;
 
 import com.example.common.entity.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
-    Result auth() {
-        return new Result();
+
+    @Value("${info}")
+    private String info;
+
+    @RequestMapping("info")
+    public Result auth() {
+        return Result.success(info);
     }
 }
