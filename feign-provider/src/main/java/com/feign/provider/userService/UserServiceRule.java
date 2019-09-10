83d832bf48942b0738456c7b11035ccaf676d1eb
xujw <xujiawei@quklive.com>
2019-09-10 17:59:16 +0800

    update

--- feign-provider/src/main/java/com/feign/provider/userService/UserServiceRule.java
+++ feign-provider/src/main/java/com/feign/provider/userService/UserServiceRule.java
@@ -1,25 +1,48 @@
 package com.feign.provider.userService;
 
+import com.netflix.loadbalancer.BaseLoadBalancer;
 import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.Server;
+
+import java.util.List;
+import java.util.Random;
 
 /**
  * 自定义负载均衡策略配置
  */
 public class UserServiceRule implements IRule {
+
+    ILoadBalancer balancer = new BaseLoadBalancer();
+
     @Override
-    public Server choose(Object o) {
+    public Server choose(Object key) {
+        List<Server> allServers = balancer.getAllServers();
+        Random random = new Random();
+        final int number = random.nextInt(10);
+        if (number < 7) {
+            return findServer(allServers, 8501);
+        }
+        return findServer(allServers, 8502);
+    }
+
+    private Server findServer(List<Server> allServers, int port) {
+        for (Server server : allServers) {
+            if (server.getPort() == port) {
+                return server;
+            }
+        }
+        System.out.println("NULL port=" + port);
         return null;
     }
 
     @Override
-    public void setLoadBalancer(ILoadBalancer iLoadBalancer) {
-
+    public void setLoadBalancer(ILoadBalancer lb) {
+        this.balancer = lb;
     }
 
     @Override
     public ILoadBalancer getLoadBalancer() {
-        return null;
+        return this.balancer;
     }
 }
