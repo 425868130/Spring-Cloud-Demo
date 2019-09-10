42d601b16ae3aa24d99f2f54e93c2e95c721dca5
xujw <xujiawei@quklive.com>
2019-09-10 14:02:37 +0800

    update

--- auth-center/src/main/java/com/example/authcenter/controller/AuthController.java
+++ auth-center/src/main/java/com/example/authcenter/controller/AuthController.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+@RefreshScope
 @RestController
 public class AuthController {
     @Autowired
--- user-service/src/main/java/com/example/userservice/controller/InfoController.java
+++ user-service/src/main/java/com/example/userservice/controller/InfoController.java
@@ -3,24 +3,28 @@ package com.example.userservice.controller;
 import com.example.common.entity.Result;
 import com.example.userservice.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RefreshScope
 @RestController
 @RequestMapping("user")
 public class InfoController {
     @Autowired
     HttpServletRequest httpServletRequest;
-
+    @Value("${base}")
+    String base;
 
     @RequestMapping("info")
     public Result getInfo() {
         User user = new User();
         user.setId(111111L);
         user.setAge(20);
-        user.setName("hello");
+        user.setName(base);
         return Result.success(user);
     }
 }
