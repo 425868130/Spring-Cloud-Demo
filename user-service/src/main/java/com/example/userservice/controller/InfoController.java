6b4fc61a9a17c4854df5d15e957ddc640d65dd75
xujw <xujiawei@quklive.com>
2019-09-10 11:57:33 +0800

    优化Maven依赖

--- user-service/src/main/java/com/example/userservice/controller/InfoController.java
+++ user-service/src/main/java/com/example/userservice/controller/InfoController.java
@@ -2,12 +2,18 @@ package com.example.userservice.controller;
 
 import com.example.common.entity.Result;
 import com.example.userservice.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("user")
 public class InfoController {
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
 
     @RequestMapping("info")
     public Result getInfo() {
