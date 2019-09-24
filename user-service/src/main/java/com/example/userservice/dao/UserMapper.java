22b77336cf200342b6277745263f8f09d41574d5
xujw <xujiawei@quklive.com>
2019-09-24 17:06:29 +0800

    update

--- user-service/src/main/java/com/example/userservice/dao/UserMapper.java
+++ user-service/src/main/java/com/example/userservice/dao/UserMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 public interface UserMapper {
-    @Select("select * from user-service")
+    @Select("select * from user")
     List<User> list();
 }
