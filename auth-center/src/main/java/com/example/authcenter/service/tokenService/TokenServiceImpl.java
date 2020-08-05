e52e2d81a00731a9aa4c8090d9313c81c3d5478a
xujw <xujiawei@quklive.com>
2020-08-05 15:05:55 +0800

    update

--- auth-center/src/main/java/com/example/authcenter/service/tokenService/TokenServiceImpl.java
+++ auth-center/src/main/java/com/example/authcenter/service/tokenService/TokenServiceImpl.java
@@ -1,13 +1,11 @@
 package com.example.authcenter.service.tokenService;
 
+import com.example.common.config.AccountAuthCfg;
 import com.example.common.config.SystemCfg;
 import com.example.common.define.ConstVal;
 import com.example.common.define.jwt.JwtPayload;
 import com.example.common.util.jwt.JwtRs256Util;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -20,29 +18,23 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
-@RefreshScope
 public class TokenServiceImpl implements TokenService {
-    private final String authPrivateKey;
-    private final String authPublicKey;
-    @Value("${system.security.account.privateKey}")
-    private String privateKey;
+    private final AccountAuthCfg accountAuthCfg;
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
     public TokenServiceImpl(SystemCfg systemCfg) {
-        this.authPrivateKey = systemCfg.getSecurity().getAccount().getPrivateKey();
-        this.authPublicKey = systemCfg.getSecurity().getAccount().getPublicKey();
-        System.out.println("privateKey:" + privateKey);
+        this.accountAuthCfg = systemCfg.getSecurity().getAccount();
     }
 
     @Override
     public Optional<String> generateToken(JwtPayload jwtPayload) {
-        return JwtRs256Util.createJWT(authPrivateKey, jwtPayload, ConstVal.Token.EXPIRES);
+        return JwtRs256Util.createJWT(accountAuthCfg.getPrivateKey(), jwtPayload, ConstVal.Token.EXPIRES);
     }
 
     @Override
     public Optional<JwtPayload> parseJWT(String tokenStr) {
-        return JwtRs256Util.parseJWT(tokenStr, authPublicKey);
+        return JwtRs256Util.parseJWT(tokenStr, accountAuthCfg.getPublicKey());
     }
 
     /**
@@ -83,7 +75,7 @@ public class TokenServiceImpl implements TokenService {
      */
     @Override
     public boolean inBlackList(String tokenStr) {
-        Optional<JwtPayload> payload = JwtRs256Util.parseJWT(tokenStr, authPublicKey);
+        Optional<JwtPayload> payload = JwtRs256Util.parseJWT(tokenStr, accountAuthCfg.getPublicKey());
         return inBlackList(payload.orElse(null));
     }
 
